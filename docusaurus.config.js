bbcaba9dc07f00978477cc62620b52d7805e1ab2
elinesnijder <a84785@ualg.pt>
2023-12-20 12:46:18 +0000

    adicionei logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,7 +68,7 @@ const config = {
         title: 'OFICINA RODAS E PNEUS',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo1.png',
         },
         items: [
           {
