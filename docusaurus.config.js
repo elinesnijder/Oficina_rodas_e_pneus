cbdc5c68b294a2519e671ed48bf75980a65b555d
elinesnijder <a84785@ualg.pt>
2023-12-20 12:40:01 +0000

    adicionei social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,7 +63,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/Social card.png',
       navbar: {
         title: 'OFICINA RODAS E PNEUS',
         logo: {
