67c61e64f964520568e4f6e56dd376bcfd1c02d4
elinesnijder <a84785@ualg.pt>
2023-12-21 18:34:17 +0000

    mudei link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Serviç
+            to="/docs/category/oficina">
+            Serviço
           </Link>
         </div>
       </div>
