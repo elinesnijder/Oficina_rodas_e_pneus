90bf03dfa947a2cf91bb194c3629db491440be47
elinesnijder <a84785@ualg.pt>
2023-12-21 17:58:01 +0000

    editei docs para oficina

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: {
+        oficina: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
