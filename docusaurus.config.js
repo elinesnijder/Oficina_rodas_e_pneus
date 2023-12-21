f552e34abe8256e8069d6311d1f0d517eddc9de8
elinesnijder <a84785@ualg.pt>
2023-12-21 15:51:20 +0000

    mudei nomes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Serviços',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Tutorial
+            Serviços
           </Link>
         </div>
       </div>
